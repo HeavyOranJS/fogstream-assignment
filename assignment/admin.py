85f6937cb8f045caa76ab7d755af0e22c036eb44
HeavyJS <whereorange@gmail.com>
2019-07-14 15:22:56 +1000

    added MessageLog structure

--- assignment/admin.py
+++ assignment/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import MessageLog
 
-# Register your models here.
+@admin.register(MessageLog)
+class MessageLogAdmin(admin.ModelAdmin):
+    # fields = ['sender login', 'time sent', 'status']
+    # fieldsets = [
+    #     ('sender login', 'sender'),
+    #     ('time sent', 'time'),
+    #     ('is successful', 'status'),
+    # ]
+
+    fieldsets = [
+        (None, {'fields': ['sender', 'status']}),
+        ('Date information', {
+            'fields': ['time']
+            }
+        )
+    ]
+
+    list_display = (
+        'sender',
+        'time',
+        'status',
+    )
--- assignment/models.py
+++ assignment/models.py
@@ -1,2 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User, UserManager
+
+class MessageLog(models.Model):
+    """
+    Stores data about sent emails: username of sender, time and boolean
+    status (True = success, False = failure)
+    """
+    #username
+    sender = models.CharField(max_length=200)
+    time = models.DateTimeField('date of sending')
+    #was email send successfully
+    status = models.BooleanField()
