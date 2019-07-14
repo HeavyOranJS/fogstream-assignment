9ee2c89aed5111f0ec66f301b0c6a9448f9a4f75
HeavyJS <whereorange@gmail.com>
2019-07-14 15:59:28 +1000

    added MessageLogManager for MessageLog object creation

--- assignment/models.py
+++ assignment/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
+class MessageLogManager(models.Manager):
+    """
+    Manager for MessageLog. Handles creation of new MessageLog objects
+    """
+    def create_messagelog(self, sender, time, status):
+        """
+        Create new MessageLog object
+        """
+        messagelog = self.create(sender=sender, time=time, status=status)
+        return messagelog
+
 class MessageLog(models.Model):
     """
     Stores data about sent emails: username of sender, time and boolean
@@ -8,5 +19,16 @@ class MessageLog(models.Model):
     #username
     sender = models.CharField(max_length=200)
     time = models.DateTimeField('date of sending')
+
+    #bind manager
+    objects = MessageLogManager()
+
     #was email send successfully
     status = models.BooleanField()
+
+    def __str__(self):
+        return "sender: {}, time: {}, status:{}".format(
+            self.sender,
+            self.time,
+            self.status
+        )
