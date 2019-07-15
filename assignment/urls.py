d7aa79905138848e2f7f409745fec46e00ed775f
HeavyJS <whereorange@gmail.com>
2019-07-15 23:57:20 +1000

    signup page now is index page

--- assignment/urls.py
+++ assignment/urls.py
@@ -6,6 +6,6 @@ app_name = 'assignment'
 
 urlpatterns = [
     path('contact/', views.ContactView.as_view(), name='contact'),
-    path('signup/', views.SignupView.as_view(), name='signup'),
     path('login/', views.LoginView.as_view(), name='login'),
+    path('', views.SignupView.as_view(), name='signup'),
 ]
