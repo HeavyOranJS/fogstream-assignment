0cb8e09268b1412d394726e235c58e405c3a8607
HeavyJS <whereorange@gmail.com>
2019-07-15 16:25:56 +1000

    now staff members reieve emails, not superusers

--- assignment/forms.py
+++ assignment/forms.py
@@ -1,10 +1,12 @@
+import logging
 from smtplib import SMTPException
-from django.utils import timezone
 
 from django import forms
-from django.core.mail import send_mail
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.utils import timezone
+
 from .models import MessageLog
 
 
@@ -14,6 +16,8 @@ class ContactForm(forms.Form):
     """
     email = forms.EmailField()
     message = forms.CharField(widget=forms.Textarea)
+
+    logger = logging.getLogger(__name__)
 
     def send_email(self, username=None):
         """
@@ -25,28 +29,28 @@ class ContactForm(forms.Form):
         email = self.cleaned_data['email']
         message = self.cleaned_data['message']
 
-        superusers = User.objects.filter(is_superuser=True)
-        superusers_emails = [superuser.email for superuser in superusers]
-        
+        #get all superuser's emails in list
+        staff_members = User.objects.filter(is_staff=True)
+        staff_members_emails = [staff.email for staff in staff_members]
+
         successfully_sent = False
         try:
-            if not superusers_emails:
+            if not staff_members_emails:
                 raise ValueError("No superusers in system or they dont have emails")
-            
+
             send_mail(
                 subject="Message from user {}".format(username),
                 message="Message:'{0}' \nEntered email: {1}".format(message, email),
                 from_email=settings.EMAIL_HOST_USER,
-                recipient_list=superusers_emails
+                recipient_list=staff_members_emails
             )
 
         except SMTPException as ex:
-            pass
-        except ValueError as ex:
-            print(ex)
+            self.logger.error('Send_email error caused by %s', ex)
+        except ValueError:
+            self.logger.error("There is no staff members or none of them have emails")
         else:
             successfully_sent = True
         finally:
             MessageLog.objects.create_messagelog(username, timezone.now(), successfully_sent)
         return successfully_sent
-        
