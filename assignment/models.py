c83ad400496d7d975693b3987e37dd82eea77757
HeavyJS <whereorange@gmail.com>
2019-07-23 21:05:08 +1000

    #1 added verbose_names, resolved merge conflict

--- assignment/models.py
+++ assignment/models.py
@@ -16,14 +16,20 @@ class MessageLog(models.Model):
     Stores data about sent emails: username of sender, time and boolean
     status (True = success, False = failure)
     """
+
+    class Meta:
+        verbose_name = "Message sent to staff"
+        verbose_name_plural = "Messages sent to staff"
+
     #assumes username
-    sender = models.CharField(max_length=200)
+    sender = models.CharField("Sender's username", max_length=200)
     time = models.DateTimeField("Date the message was sent")
     #was email sent successfully
     status = models.BooleanField("Sent successfully")
 
     #bind manager
     objects = MessageLogManager()
+
 
     def __str__(self):
         return "sender: {}, time: {}, status:{}".format(
